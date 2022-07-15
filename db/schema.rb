fb964a1c96ea37ed9a226d2241e4ddcd19949859
Benjamin Saulnier <bsaulnier@cmmc02zx06dlvdg.innova.local>
2022-07-15 19:42:50 -0400

    removed product_id from product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_14_190130) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_15_233539) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,7 +37,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_14_190130) do
     t.decimal "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "product_id"
   end
 
   create_table "users", force: :cascade do |t|
