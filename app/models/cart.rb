2cff00f53fe32f0ddbebf90bd22b18dce9e15363
Benjamin Saulnier <bsaulnier@cmmc02zx06dlvdg.innova.local>
2022-07-16 19:34:40 -0400

    cart no longer deleting products

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,7 +1,6 @@
 class Cart < ApplicationRecord
-  has_many :orders
+  has_many :orders, dependent: :destroy
   has_many :products, through: :orders
-
   def total
     orders.to_a.sum { |order| order.total }
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Product.destroy_all
+
+
+Product.create!([{
+  name: "Bearded Dragon"
+  description: "Some kind of Wizard Lizard"
+  price: 100.00
+  our_price: 89.99 
+  rating: 3
+}
+])
