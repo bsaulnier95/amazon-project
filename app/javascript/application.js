8c54eadded282c0bc769531d7ce90bb3b58d60f5
Benjamin Saulnier <bsaulnier@cmmc02zx06dlvdg.innova.local>
2022-07-16 21:24:31 -0400

    Active Storage

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,4 +5,5 @@ import * as bootstrap from "bootstrap"
 let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))  
 let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {  
   return new bootstrap.Popover(popoverTriggerEl)  
-})  
+})  import "trix"
+import "@rails/actiontext"
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+  has_one_attached :image
   has_many :orders
   has_many :carts, through: :orders
 end
