90afb0f591a95be45ce08f636e317086f52239b0
Benjamin Saulnier <bsaulnier@cmmc02zx06dlvdg.innova.local>
2022-07-18 21:24:37 -0400

    Added Admin, Hide functions for non-admins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :index
+
   before_action :set_render_cart
   before_action :initialize_cart
 
