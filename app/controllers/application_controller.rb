ae10d4407575463b399d74b31d3d602191878eba
Benjamin Saulnier <bsaulnier@cmmc02zx06dlvdg.innova.local>
2022-07-19 13:55:55 -0400

    added null:false to added user fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!, except: :index
-
   before_action :set_render_cart
   before_action :initialize_cart
 
@@ -10,10 +11,22 @@ class ApplicationController < ActionController::Base
 
   def initialize_cart
     @cart ||= Cart.find_by(id: session[:cart_id])
-
     if @cart.nil?
       @cart = Cart.create
       session[:cart_id] = @cart.id
+    end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user|
+      user.permit(:name, :email, :password, :first_name, :last_name, :address, :city, :state, :zip_code)
+    end
+
+    devise_parameter_sanitizer.permit(:account_update) do |user|
+      user.permit(:name, :email, :password, :current_password, :first_name, :last_name, :address, :city, :state,
+                  :zip_code)
     end
   end
 end
--- db/migrate/20220719140820_add_fields_to_users.rb
+++ db/migrate/20220719140820_add_fields_to_users.rb
@@ -1,10 +1,10 @@
 class AddFieldsToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :first_name, :string, null: false
-    add_column :users, :last_name, :string, null: false
-    add_column :users, :address, :string, null: false
-    add_column :users, :city, :string, null: false
-    add_column :users, :state, :string, null: false
-    add_column :users, :zip_code, :integer, null: false
+    add_column :users, :first_name, :string, default: '', null: false
+    add_column :users, :last_name, :string, default: '', null: false
+    add_column :users, :address, :string, default: '', null: false
+    add_column :users, :city, :string, default: '', null: false
+    add_column :users, :state, :string, default: '', null: false
+    add_column :users, :zip_code, :string, default: '', null: false
   end
 end
