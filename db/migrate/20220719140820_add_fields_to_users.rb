fb8caa821bbfe16556b05454d385563d00e77143
Benjamin Saulnier <bsaulnier@cmmc02zx06dlvdg.innova.local>
2022-07-19 10:11:39 -0400

    Before fields added to user migration

---
+++ db/migrate/20220719140820_add_fields_to_users.rb
@@ -0,0 +1,10 @@
+class AddFieldsToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :first_name, :string, null: false
+    add_column :users, :last_name, :string, null: false
+    add_column :users, :address, :string, null: false
+    add_column :users, :city, :string, null: false
+    add_column :users, :state, :string, null: false
+    add_column :users, :zip_code, :integer, null: false
+  end
+end
