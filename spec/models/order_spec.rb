549020da6ff3759b3e2f13f33a717fa1dafbeaee
Benjamin Saulnier <bsaulnier@cmmc02zx06dlvdg.innova.local>
2022-08-06 19:24:10 -0400

    Cart Model Test

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -7,4 +7,10 @@ RSpec.describe Order, type: :model do
     order = create(:order, product_id: product.id, quantity: 2)
     expect(order.total).to eq(200)
   end
+  it 'Creates total based on product quantity' do
+    create(:cart)
+    product = create(:product, name: 'test', our_price: 200)
+    order = create(:order, product_id: product.id, quantity: 5)
+    expect(order.total).to eq(1000)
+  end
 end
